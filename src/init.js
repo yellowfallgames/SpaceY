ab63e11a39fbcbd3cd32bd9a96eb9e94936cddec
PepePm <pepejedi.pp@gmail.com>
2020-11-07 17:31:14 +0100

    Update init.js

--- src/init.js
+++ src/init.js
@@ -1,17 +1,66 @@
-///*
+/*
+//import { game } from './src/init.js';
+
+var Escena1 = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+        function Escena1(){
+            Phaser.Scene.call(this, {key: 'Escena1'});
+        },
+    create(){
+        console.log('Creando escena 1');
+        texto = this.add.text(game.config.width/2, game.config.height/2, 'Esta es la escena PRIMERA.',{
+            fontsize:'40 px',
+            fill: '#ffffff'
+        }).setOrigin(0.5).setInteractive();
+    
+
+        texto.on('pointerdown',()=>{
+            console.log('Cambiando a escena 2');
+            this.scene.start('Escena2');
+        });
+    }
+})
+
+var Escena2 = new Phaser.Class({
+    Extends: Phaser.Scene,
+    initialize:
+        function Escena2(){
+            Phaser.Scene.call(this, {key: 'Escena2'});
+        },
+    create(){
+        console.log('Creando escena 2');
+        texto = this.add.text(game.config.width/2, game.config.height/2, 'Esta es la escena SEGUNDA.',{
+            fontsize:'40 px',
+            fill: '#ffffff'
+        }).setOrigin(0.5).setInteractive();
+
+        texto.on('pointerdown',()=>{
+            console.log('Cambiando a escena 1');
+            this.scene.start('Escena1');
+        });
+    }
+})
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 720,
+    parent: 'Escenas',
+    scene: [Escena1,Escena2]
+};
+
+var game = new Phaser.Game(config);
+
 
 //*/
-
+/*
 var config = {
     width: 1024,
     height: 720,
     parent: "container",
     type: Phaser.AUTO,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    },
+    scene: [Escena1, Escena2],
     physics: {
         default: "arcade",
         arcade: {
@@ -24,6 +73,8 @@ var config = {
 }
 
 var game = new Phaser.Game(config);
+//*/
+
 var directory = "./Resources/Game/";
 //Objetos
 var player;
@@ -31,7 +82,7 @@ var marte;
 var terraformador;
 
 //Recursos
-var materiales = 100;
+var numMateriales = 0;
 var objMateriales;
 var MAX_MATERIALES = 100;
 var materiales_color = Phaser.Display.Color.GetColor(57, 182, 188);
@@ -39,6 +90,7 @@ var materiales_color = Phaser.Display.Color.GetColor(57, 182, 188);
 //Inputs
 var key_left;
 var key_right;
+var key_interact;
 
 function preload() {
     
@@ -55,10 +107,12 @@ function create() {
     marte = this.add.image(220, 650, "marte");
     terraformador = this.physics.add.sprite(220, 655, "terraformador");
     player = this.physics.add.sprite(220, 335, 'vulpin_idle');
-    console.log(player);
+
     objMateriales = this.add.sprite(10, 50, "barra");
     objMateriales.setOrigin(0, 0.5);
-    objMateriales.setScale(materiales/MAX_MATERIALES, 0.3);
+    objMateriales.setScale(numMateriales/MAX_MATERIALES, 0.3);
+    objMateriales.tint = materiales_color;//Math.random() * 0xffffff;
+    console.log(objMateriales.tint);
 
     player.setScale(1.5);
     marte.setScale(1.5);
@@ -66,8 +120,6 @@ function create() {
     terraformador.setRotation(0.5);
 
     //this.physics.add.overlap(player, terraformador, colliderInteract);
-    objMateriales.tint = materiales_color;//Math.random() * 0xffffff;
-    console.log(objMateriales.tint);
 
     //Animations
     this.anims.create({
@@ -87,6 +139,7 @@ function create() {
     this.cursors = this.input.keyboard.createCursorKeys();
     key_left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     key_right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    key_interact = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
 
     console.log(Phaser.Input.Keyboard.KeyCodes);
     console.log(marte);
@@ -125,12 +178,19 @@ function update(time, delta) {
     else {
 
         player.anims.play('vulpin_idle', true);
-    }       
-}
+    }
 
-function colliderInteract() {
+    //Interaccionar
+    if (key_interact.isDown) {
 
-    console.log("A");
+        if (terraformador.rotation > -0.15 && terraformador.rotation < 0.15) {
+            if (numMateriales < MAX_MATERIALES) {
+
+                numMateriales++;
+                objMateriales.scaleX = numMateriales/MAX_MATERIALES;
+            } 
+        }
+    }
 }
 
 //*/
