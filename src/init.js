99cd0994695cdec91e649f49d3de9855728440e2
manutoarts <manutoarts@gmail.com>
2020-11-12 21:15:19 +0100

    UI MONTADA
    Falta:
    Añadir oxigeno,
    añadir UI Marte barra O2
    añadir UI Marte barra ROCAS
    
    Añadir COHETE no se ve

---
+++ src/init.js
@@ -0,0 +1,24 @@
+var config = {
+    width: 1600,
+    height: 900,
+    parent: "container",
+    type: Phaser.AUTO,
+    /*scene: {
+        preload: preload,
+        create: create,
+        update: update     
+    },//*/
+    scene:[SceneMenu, SceneGame, SceneContact, SceneOptions],
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 0,
+                debug: false
+            }
+        }
+    }
+}
+
+var game = new Phaser.Game(config);
+
